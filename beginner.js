c1fd5c590e744299fcf60fd9a39beaccd81b6913
John Potts <john@arlingtonchurch.com>
2017-08-31 08:18:24 -0400

    Update beginner.js

--- beginner.js
+++ beginner.js
@@ -19,3 +19,17 @@ function palindrome(str){
 
 
 palindrome("eye");
+
+/*Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted.
+The returned value should be a number.
+For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1). */
+
+
+function getIndexToIns(arr,value){
+  arr.push(value);
+  arr.sort(function(a,b){return a-b;});
+  return arr.indexOf(value);
+           
+}
+  
+getIndexToIns([40, 60], 50);
