f7116e475033ff9c768a037723387d1afb52bfe3
John Potts <john@arlingtonchurch.com>
2017-09-08 05:25:15 -0400

    Update beginner.js

--- beginner.js
+++ beginner.js
@@ -54,3 +54,23 @@ function getIndexToIns(arr,value){
 }
   
 getIndexToIns([40, 60], 50);
+
+
+/*Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
+
+Note that inserting the three dots to the end will add to the string length.
+
+However, if the given maximum string length num is less than or equal to 3, then the addition of the three dots does not add to the string length in determining the truncated string.*/
+//done
+function truncateString(str, num) {
+ if (str.length>num)
+ { if (num<4)
+    {return str.slice(0,num) + "...";}
+  else{
+  var tr = str.slice(0,num-3);
+    return tr+"...";
+}
+}
+else{return str;}
+}
+truncateString("A-tisket a-tasket A green and yellow basket", 11);
